3096b02fc56dda5c619c2ba0a7029e4a356697d2
Fuongbregas <fuongbregas@icloud.com>
2019-11-16 17:53:11 -0800

    Not sure what I did

--- getAllTweetFollowing.js
+++ getAllTweetFollowing.js
@@ -3,18 +3,19 @@ const TweetSchema = require("./TweetSchema");
 exports.getAllTweetFollowing = (req, res) => {
 
     console.log("Find the array of following");
-    
+
 
     console.log("Inside getAllTweet of Following");
     var tweetOwnerUsername = req.session.username;
     TweetSchema.find({ username: tweetOwnerUsername }, (err, result) => {
+        var tweets = result.tweets; // array of tweets
         if (err) {
             throw err;
         } else {
             res.writeHead(200, {
                 "content-type": "application/json"
               });
-              res.end(JSON.stringify(result));
+              res.end(JSON.stringify(tweets));
         } 
     });
 };
